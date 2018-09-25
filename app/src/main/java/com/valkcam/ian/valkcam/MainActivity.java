fc0ce962699b97be842a8312d16bc3e08dc481bf
SpyGuyIan <spyguyian@gmail.com>
2018-09-24 21:39:31 -0400

    Hid buttons, everything is working well. too well.......

--- app/src/main/java/com/valkcam/ian/valkcam/MainActivity.java
+++ app/src/main/java/com/valkcam/ian/valkcam/MainActivity.java
@@ -6,13 +6,15 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class MainActivity extends Activity {
-    String piAddr = "http://192.168.1.33:8000/index.html";
+    String piAddr = "http://192.168.4.1:8000/index.html";
 
 
     @Override
@@ -25,13 +27,24 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         WebView mWebView = findViewById(R.id.webview);
+        ImageButton btnTop = findViewById(R.id.btnTop);
+        ImageButton btnBottom = findViewById(R.id.btnBottom);
+        ImageButton btnLeft = findViewById(R.id.btnLeft);
+        ImageButton btnRight = findViewById(R.id.btnRight);
+
+        btnTop.setVisibility(View.INVISIBLE);
+        btnBottom.setVisibility(View.INVISIBLE);
+        btnLeft.setVisibility(View.INVISIBLE);
+        btnRight.setVisibility(View.INVISIBLE);
+
         // disable scroll on touch
-        mWebView.setOnTouchListener(new View.OnTouchListener() {
+        /*mWebView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 return (event.getAction() == MotionEvent.ACTION_MOVE);
             }
-        });
+        });*/
+
         mWebView.loadUrl(piAddr);
 
     }

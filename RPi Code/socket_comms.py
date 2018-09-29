32df1b3a352a97ceebb2a83cd679d2d71f8497b8
Ian <spyguyian@gmail.com>
2018-09-29 19:32:07 -0400

    Created full python script to be run from raspberry pi

--- RPi Code/socket_comms.py
+++ RPi Code/socket_comms.py
@@ -1,19 +1,196 @@
-#!/usr/bin/python           
+import socket
+from _thread import *
+import ast
+import io
+import picamera
+import logging
+import socketserver
+from threading import Condition
+from http import server
+import time
+import wiringpi
+from noise import pnoise1
 
-import socket               
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 5000                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+"""
+ToDo:
+Save state of otions and send them out on socket connect
+More camera settings like low light etc
+"""
+MAX_SERVO = 250;
+MIN_SERVO = 50;
+MAIN_LOOP_DELAY = 0.01
+PERLIN_SPEED = 2500     #the larger the smmoother
 
-s.listen(5)                 # Now wait for client connection.
+varDict = {'x':0,
+           'y':0,
+           'mode':0,    #0:manual, 1:perlin, 2:
+           'quality':"720p", 
+           'fps':30,
+           'update':1}
 
-try:
+#COde to handle communication with client sockets
+def threaded_comms(soc, addr):
+    data = ''
+    msg = ''
     while True:
-       c, addr = s.accept()     # Establish connection with client.
-       print 'Got connection from', addr
-       c.send('Thank you for connecting')
-       c.close()                # Close the connection
-except KeyboardInterrupt:
-    exit()
+        data = soc.recv(16)
+        if not data:
+            break
+        msg = data.decode(encoding='UTF-8')
+        msg = msg.replace("{", "{\'").replace("=", "\': ").rstrip()    
+        print(addr[0] + ":" + msg)
+        d = ast.literal_eval(msg);
+        for key in varDict:
+            if key in d and (key == 'x' or key == 'y'):
+                varDict[key] += d[key]
+                varDict[key] = max(MIN_SERVO, min(MAX_SERVO, varDict[key]))
+            elif key in d:
+                varDict[key] = d[key]                
+        
+#Setup socket        
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+try:
+    s.bind((socket.gethostname(), 5000))
+except socket.error as err:
+    print('Bind failed. Error Code : ' .format(err))
+s.listen(4)
+print("Socket Listening")
+
+#create thread to handle listening for clients
+def create_clients:
+    while True:
+        conn, addr = s.accept()
+        print("Connection extablished: ", addr)
+        start_new_thread(threaded_comms,(conn,addr));
+start_new_thread(create_clients)
+
+# Web streaming example
+# Adapted from source code in the official PiCamera package
+# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
+
+PAGE="""\
+<style>
+html,body{
+    margin:0;
+    height:100%;
+}
+img{
+  display:block;
+  width:100%; height:100%;
+  object-fit: cover;
+}
+</style>
+<html>
+<body>
+<img src="stream.mjpg" alt="">
+</html>
+"""
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+#setup camera (with block ensures nice cleanup)
+with picamera.PiCamera(resolution='1280x720', framerate=24) as camera
+    output = StreamingOutput()
+    #camera.rotation = 90
+    camera.start_recording(output, format='mjpeg')
+    def run_streaming():
+        try:
+            address = ('', 8000)
+            server = StreamingServer(address, StreamingHandler)
+            server.serve_forever()
+        finally:
+            camera.stop_recording()
+    start_new_thread(run_streaming)
+
+    #setup servos
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.pinMode(13, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+    # divide down clock for servos
+    wiringpi.pwmSetClock(192)
+    wiringpi.pwmSetRange(2000)
+
+    #handle servos and settings changes
+    startMillis = int(round(time.time() * 1000))
+    while True:
+        time.sleep(MAIN_LOOP_DELAY)
+        if varsDict['mode'] = 0:
+            wiringpi.pwmWrite(18, varsDict['x'])
+            wiringpi.pwmWrite(13, varsDict['y'])
+        elif varsDict['mode'] = 1:
+            millis = int(round(time.time() * 1000))
+            x = pnoise1((startMillis-millis)/PERLIN_SPEED+1234.56789, octaves=4)
+            y = pnoise1((startMillis-millis)/PERLIN_SPEED, octaves=4)
+            wiringpi.pwmWrite(18, (x+1)/2*200+50)
+            wiringpi.pwmWrite(13, (y+1)/2*200+50)
+            
+        if varsDict['update'] != 0:
+            varsDict['update'] = 0
+            camera.stop_recording()
+            camera.resolution = varsDict['quality']
+            camera.framerate = varsDict['fps']
+            camera.start_recording(output, format='mjpeg')
+        
+
+
+                           
--- app/src/main/java/com/valkcam/ian/valkcam/MainActivity.java
+++ app/src/main/java/com/valkcam/ian/valkcam/MainActivity.java
@@ -2,59 +2,61 @@ package com.valkcam.ian.valkcam;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetAddress;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 /**
  * Todo:
  *  Make on resume refresh webpage
- *  Options: quality, restart serv, random moovements on/off
+ *  Options: quality, restart serv, random moovements on/off, auto wb etc.
  *  Send button data
  */
 public class MainActivity extends Activity {
 
-    private Socket socket;
-
+    private static final long SOCKET_SEND_DELTA = 100;//ms in between sending
     private static final int SERVERPORT = 5000;
-    private static final String SERVER_IP = "192.168.4.1";
+    private static final String SERVER_IP = "192.168.1.36";
     private static final String piAddr = "http://192.168.4.1:8000/index.html";
-
+    private CommHandler cThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        cThread = new CommHandler();
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
 
         WebView mWebView = findViewById(R.id.webview);
-        ImageButton btnTop = findViewById(R.id.btnTop);
+        final ImageButton btnTop = findViewById(R.id.btnTop);
         ImageButton btnBottom = findViewById(R.id.btnBottom);
         ImageButton btnLeft = findViewById(R.id.btnLeft);
         ImageButton btnRight = findViewById(R.id.btnRight);
-
-        btnTop.setVisibility(View.INVISIBLE);
-        btnBottom.setVisibility(View.INVISIBLE);
-        btnLeft.setVisibility(View.INVISIBLE);
-        btnRight.setVisibility(View.INVISIBLE);
+        final Activity This = this;
 
         btnTop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                cThread.setVar("x", (int)(Math.random()*100));
+
+                Toast.makeText(This, "test", Toast.LENGTH_SHORT).show();
 
             }
         });
+
 
         // disable scroll on touch
         /*mWebView.setOnTouchListener(new View.OnTouchListener() {
@@ -65,25 +67,57 @@ public class MainActivity extends Activity {
         });*/
 
         mWebView.loadUrl(piAddr);
-        new Thread(new ClientThread()).start();
-
     }
 
-    class ClientThread implements Runnable {
+
+
+
+    class CommHandler extends Thread{
+
+        private HashMap<String, Integer> variables = new HashMap<>();
+        private boolean updated = false;
+
+        public CommHandler() {
+            super();
+            this.start();
+        }
 
         @Override
         public void run() {
+            super.run();
+
+            Socket socket;
+            OutputStream out;
+            PrintWriter output;
 
             try {
-
                 socket = new Socket(SERVER_IP, SERVERPORT);
+                out = socket.getOutputStream();
+                output = new PrintWriter(out);
 
-            } catch (UnknownHostException e1) {
+                long sentmillis = System.currentTimeMillis();
+                while(!interrupted()){
+                    if(System.currentTimeMillis() - sentmillis > SOCKET_SEND_DELTA && updated && !variables.isEmpty()){
+                        sentmillis = System.currentTimeMillis();
+                        output.println(variables.toString());
+                        output.flush();
+                        updated = false;
+                    }
+                }
+
+                socket.close();
+                output.close();
+                out.close();
+            } catch (Exception e1) {
                 e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
+
             }
+        }
 
+
+        public void setVar(String s, int i){
+            variables.put(s, i);
+            updated = true;
         }
 
     }
